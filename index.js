9d1416841c2b9d5296dce6e11adafb515eb1ecf6
Sam MacPherson <xylom44@gmail.com>
2017-11-03 17:26:58 -0400

    bug fixes

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ var bitcoin = require('bitcoinjs-lib');
 var request = require('superagent');
 
 var BITCOIN_DIGITS = 8;
+var BITCOIN_SAT_MULT = Math.pow(10, BITCOIN_DIGITS);
 
 var providers = {
+	/**
+	 * Input: Address to retrieve the balance from.
+	 * Output: The balance in Satoshis.
+	 */
+	balance: {
+		mainnet: {
+			blockexplorer: function (addr) {
+				return request.get('https://blockexplorer.com/api/addr/' + addr + '/balance').send().then(function (res) {
+					return parseFloat(res.body);
+				});
+			},
+			blockchain: function (addr) {
+				return request.get('https://blockchain.info/q/addressbalance/' + addr + '?confirmations=6').send().then(function (res) {
+					return parseFloat(res.body);
+				});
+			}
+		},
+		testnet: {
+			blockexplorer: function (addr) {
+				return request.get('https://testnet.blockexplorer.com/api/addr/' + addr + '/balance').send().then(function (res) {
+					return parseFloat(res.body);
+				});
+			}
+		}
+	},
 	/**
 	 * Input: Requested processing speed. "fastest", "halfHour" or "hour"
 	 * Output: Fee rate in Satoshi's per Byte.
@@ -12,14 +38,14 @@ var providers = {
 		mainnet: {
 			earn: function (feeName) {
 				return request.get('https://bitcoinfees.earn.com/api/v1/fees/recommended').send().then(function (res) {
-					return res[feeName + "fee"];
+					return res.body[feeName + "Fee"];
 				});
 			}
 		},
 		testnet: {
 			earn: function (feeName) {
 				return request.get('https://bitcoinfees.earn.com/api/v1/fees/recommended').send().then(function (res) {
-					return res[feeName + "fee"];
+					return res.body[feeName + "Fee"];
 				});
 			}
 		}
@@ -31,8 +57,8 @@ var providers = {
 	utxo: {
 		mainnet: {
 			blockexplorer: function (addr) {
-				return request.get('https://blockexplorer.com/api/addr/' + addr + '/utxo?noCache=1').send().then(function (utxos) {
-					return utxos.map(function (e) {
+				return request.get('https://blockexplorer.com/api/addr/' + addr + '/utxo?noCache=1').send().then(function (res) {
+					return res.body.map(function (e) {
 						return {
 							txid: e.txid,
 							vout: e.vout,
@@ -43,8 +69,8 @@ var providers = {
 				});
 			},
 			blockchain: function (addr) {
-				return request.get('https://blockchain.info/unspent?active=' + addr).send().then(function (utxos) {
-					return utxos.map(function (e) {
+				return request.get('https://blockchain.info/unspent?active=' + addr).send().then(function (res) {
+					return res.body.unspent_outputs.map(function (e) {
 						return {
 							txid: e.tx_hash_big_endian,
 							vout: e.tx_output_n,
@@ -57,8 +83,8 @@ var providers = {
 		},
 		testnet: {
 			blockexplorer: function (addr) {
-				return request.get('https://testnet.blockexplorer.com/api/addr/' + addr + '/utxo?noCache=1').send().then(function (utxos) {
-					return utxos.map(function (e) {
+				return request.get('https://testnet.blockexplorer.com/api/addr/' + addr + '/utxo?noCache=1').send().then(function (res) {
+					return res.body.map(function (e) {
 						return {
 							txid: e.txid,
 							vout: e.vout,
@@ -77,7 +103,7 @@ var providers = {
 	pushtx: {
 		mainnet: {
 			blockexplorer: function (hexTrans) {
-				return request.post('https://blockexplorer.info/api/tx/send').send('rawtx: ' + hexTrans);
+				return request.post('https://blockexplorer.com/api/tx/send').send('rawtx: ' + hexTrans);
 			},
 			blockchain: function (hexTrans) {
 				return request.post('https://blockchain.info/pushtx').send('tx=' + hexTrans);
@@ -85,22 +111,37 @@ var providers = {
 		},
 		testnet: {
 			blockexplorer: function (hexTrans) {
-				return request.post('https://testnet.blockexplorer.info/api/tx/send').send('rawtx: ' + hexTrans);
+				return request.post('https://testnet.blockexplorer.com/api/tx/send').send('rawtx: ' + hexTrans);
+			},
+			blockcypher: function (hexTrans) {
+				return request.post('https://api.blockcypher.com/v1/btc/test3/txs/push').send('{"tx":"' + hexTrans + '"}');
 			}
 		}
 	}
 }
 
 //Set default providers
+providers.balance.mainnet.default = providers.balance.mainnet.blockexplorer;
+providers.balance.testnet.default = providers.balance.testnet.blockexplorer;
 providers.fees.mainnet.default = providers.fees.mainnet.earn;
 providers.fees.testnet.default = providers.fees.testnet.earn;
 providers.utxo.mainnet.default = providers.utxo.mainnet.blockexplorer;
 providers.utxo.testnet.default = providers.utxo.testnet.blockexplorer;
 providers.pushtx.mainnet.default = providers.pushtx.mainnet.blockchain;
-providers.pushtx.testnet.default = providers.pushtx.testnet.blockexplorer;
+providers.pushtx.testnet.default = providers.pushtx.testnet.blockcypher;
+
+function getBalance (addr, options) {
+	if (options == null) options = {};
+	if (options.network == null) options.network = "mainnet";
+	if (options.balanceProvider == null) options.balanceProvider = providers.balance[options.network].default;
+
+	return options.balanceProvider(addr).then(function (balSat) {
+		return balSat/BITCOIN_SAT_MULT;
+	});
+}
 
 function getTransactionSize (numInputs, numOutputs) {
-	return numInputs*180 + numInputs*34 + 10 + numInputs;
+	return numInputs*180 + numOutputs*34 + 10 + numInputs;
 }
 
 function getFees (provider, feeName) {
@@ -122,15 +163,16 @@ function sendTransaction (options) {
 	//Optionals
 	if (options.network == null) options.network = 'mainnet';
 	if (options.fee == null) options.fee = 'fastest';
-	if (options.confirmations == null) options.confirmations = 6;
 	if (options.feesProvider == null) options.feesProvider = providers.fees[options.network].default;
 	if (options.utxoProvider == null) options.utxoProvider = providers.utxo[options.network].default;
 	if (options.pushtxProvider == null) options.pushtxProvider = providers.pushtx[options.network].default;
+	if (options.dryrun == null) options.dryrun = false;
 
 	var from = options.from;
 	var to = options.to;
 	var amount = options.btc;
-	var amtSatoshi = Math.floor(amount*Math.pow(10, BITCOIN_DIGITS));
+	var amtSatoshi = Math.floor(amount*BITCOIN_SAT_MULT);
+	var bitcoinNetwork = options.network == "testnet" ? bitcoin.networks.testnet : bitcoin.networks.bitcoin;
 
 	return Promise.all([
 		getFees(options.feesProvider, options.fee),
@@ -139,7 +181,8 @@ function sendTransaction (options) {
 		var feePerByte = res[0];
 		var utxos = res[1];
 
-		var tx = new bitcoin.TransactionBuilder();
+		//Setup inputs from utxos
+		var tx = new bitcoin.TransactionBuilder(bitcoinNetwork);
 		var ninputs = 0;
 		var availableSat = 0;
 		for (var i = 0; i < utxos.length; i++) {
@@ -152,20 +195,29 @@ function sendTransaction (options) {
 				if (availableSat >= amtSatoshi) break;
 			}
 		}
+
+		if (availableSat < amtSatoshi) throw "You do not have enough in your wallet to send that much.";
+
 		var change = availableSat - amtSatoshi;
 		var fee = getTransactionSize(ninputs, change > 0 ? 2 : 1)*feePerByte;
+		if (fee > amtSatoshi) throw "BitCoin amount must be larger than the fee. (Ideally it should be MUCH larger)";
 		tx.addOutput(to, amtSatoshi - fee);
 		if (change > 0) tx.addOutput(from, change);
-		var keyPair = bitcoin.ECPair.fromWIF(options.privKeyWIP);
+		var keyPair = bitcoin.ECPair.fromWIF(options.privKeyWIP, bitcoinNetwork);
 		for (var i = 0; i < ninputs; i++) {
 			tx.sign(i, keyPair);
 		}
 		var msg = tx.build().toHex();
-		return options.pushtxProvider(msg);
+		if (options.dryrun) {
+			return msg;
+		} else {
+			return options.pushtxProvider(msg);
+		}
 	});
 }
 
 module.exports = {
 	providers: providers,
+	getBalance: getBalance,
 	sendTransaction: sendTransaction
 }
